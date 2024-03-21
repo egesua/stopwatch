56ce32c8555be12475014ed0c75eb2efc02711b9
egesua <egesu06@gmail.com>
2024-03-21 20:30:58 +0300

    functions created.

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -1,6 +1,35 @@
 import React, {useState, useEffect, useRef } from "react";
 
 function StopWatch() {
+    
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  //catch the current.
+  const intervalIdRef = useRef(null);
+
+  //start at 0.
+  const startTimeRef = useRef(0);
+
+  useEffect(() => {
+
+  },[isRunning])
+
+  function start() {
+
+  }
+
+  function stop() {
+
+  }
+
+  function reset() {
+
+  }
+
+  function formatTime() {
+    
+  }
 
     return (
         <></>
